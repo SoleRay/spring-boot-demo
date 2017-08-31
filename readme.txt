81087485ea91b00cc5941ebf5fdbd68195c06d8d
SoleRay <pein_zero@126.com>
2017-08-31 16:53:04 +0800

    re-init

--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 1.application.yml和application-{profile}.properties两者选其一
 2.mybatis选通用mybatis
-3.用sprign-data-redis集成redis的时候，默认会使用Jedis作为实现
+3.用sprign-data-redis集成redis的时候，默认会使用Jedis作为实现，只需要在yml或者properties中配置即可，无需额外配置
 4.摒弃Date,统一使用LocalDate，LocalDateTime，所以增加了mybatis和jackson的相关支持
 5.LocalDate，LocalDateTime的入参使用@DateTimeFormat标记，出参使用@JsonFormat标记（注意时区）
-6.
+6.自定义常量配置文件，采用注入的形式，详情参考demo.properties、PropDemo.java
--- src/main/java/com/demo/controller/demo/DemoController.java
+++ src/main/java/com/demo/controller/demo/DemoController.java
@@ -6,8 +6,11 @@ import com.demo.bean.result.Result;
 import com.demo.dto.param.demo.DemoParamDTO;
 import com.demo.dto.result.demo.DemoResultDTO;
 import com.demo.entity.demo.Demo;
+import com.demo.properties.demo.PropDemo;
 import com.demo.service.demo.DemoService;
 import com.demo.util.resp.ResponseUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,8 +22,13 @@ import javax.annotation.Resource;
 @RequestMapping("/demo")
 public class DemoController {
 
+    private static final Logger logger = LogManager.getLogger(DemoController.class);
+
     @Resource
     private DemoService demoService;
+
+    @Resource
+    private PropDemo propDemo;
 
     @ValidateSign
     @RequestMapping(value="/get",method= RequestMethod.POST)
@@ -33,6 +41,8 @@ public class DemoController {
         dto.setId(demo.getId());
         dto.setCreateDate(demo.getCreateDate());
 
+
+        logger.info(propDemo.getName());
         return ResponseUtil.setSuccessDataResponse(dto);
     }
 }
