adfa3529110603c4188c6b473d03f0914415fd42
soleray <pein_zero@126.com>
2018-07-04 11:11:39 +0800

    tk.mybatis更新

--- src/main/java/com/demo/dao/demo/DemoDao.java
+++ src/main/java/com/demo/dao/demo/DemoDao.java
@@ -3,5 +3,6 @@ package com.demo.dao.demo;
 import com.demo.entity.demo.Demo;
 import tk.mybatis.mapper.common.Mapper;
 
+@org.apache.ibatis.annotations.Mapper
 public interface DemoDao extends Mapper<Demo> {
 }
