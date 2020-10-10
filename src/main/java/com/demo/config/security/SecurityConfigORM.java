f10434684877f4d76878a5ea82de12ef669685b8
soleray <pein_zero@126.com>
2020-10-10 20:31:07 +0800

    update

--- src/main/java/com/demo/config/security/SecurityConfigORM.java
+++ src/main/java/com/demo/config/security/SecurityConfigORM.java
@@ -1,19 +1,21 @@
 package com.demo.config.security;
 
-import com.demo.service.user.impl.UserServiceImpl;
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -42,28 +44,56 @@ public class SecurityConfigORM extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        http.authorizeRequests()
+                .antMatchers("/box/**").hasIpAddress("127.0.0.1");//来自127.0.0.1的请求，如果匹配到"/box",那就可以不登录,直接访问
+
         //这种方式适合单机或者集群的，分布式不用这种方式
-        http.authorizeRequests().anyRequest().authenticated()
+        http.authorizeRequests()
+                .anyRequest().authenticated()//所有请求都需要认证，这个和下面放行匹配是冲突的
                 .and().formLogin()//浏览器中输入，http://localhost:8081/spring-boot-demo/login，默认登陆页
-                //.loginPage("mylogin.html") //自定义登录页
+                //.loginPage("mylogin.html") //自定义登录页，使用自定义登陆界面以后，登出界面就要重新定义
                 .loginProcessingUrl("/login")//登陆的post接口
-                .defaultSuccessUrl("/")//登陆成功后的页面
-                .failureUrl("/login.html?error")//登陆失败后，统一跳转的页面，如果需要根据失败原因跳转不同页面，配置failureHandler
-                .failureHandler(new AuthenticationFailureHandler() { //登陆失败后的处理，可以根据不同的错误原因跳转不同的页面
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        //登陆成功处理，如果前后端分离，可以直接用于json返回
+
+                        String json = "{\"code\": 0,\"message\": \"ok\"}";
+                        response.setContentType("text/json; charset=utf-8");
+                        response.getWriter().print(JSON.toJSON(json));
+                    }
+                })
+//                .defaultSuccessUrl("/")//登陆成功后的页面
+//                .failureUrl("/login.html?error")//登陆失败后，统一跳转的页面，如果需要根据失败原因跳转不同页面，配置failureHandler
+                .failureHandler(new AuthenticationFailureHandler() {
                     @Override
                     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
 
-                        //记录失败次数
+                        /**
+                         * 登陆失败后的处理，可以
+                         *    1.根据不同的错误原因跳转不同的页面
+                         *    2.打印失败信息
+                         *    3.统计失败次数
+                         */
 
+                        String msg = "error";
                         //根据失败原因跳转到不同的页面
                         if(e instanceof UsernameNotFoundException){
-                            request.getRequestDispatcher("/userNameError.html").forward(request,response);
+                            msg = e.getMessage();
                         }else if(e instanceof BadCredentialsException){
-
+                            msg = e.getMessage();
                         }
-
+                        String json = "{\"code\": 1,\"message\": \""+msg+"\"}";
+                        response.setContentType("text/json; charset=utf-8");
+                        response.getWriter().print(JSON.toJSON(json));
                     }
                 })
+                .and().logout().addLogoutHandler(new LogoutHandler() {//可以加好几个handler
+            @Override
+            public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                //在登出处理中，可以进行一些资源清理。
+            }
+        })
                 .and().csrf().disable();//巨坑：重写configure以后，默认开启了csrf，拦截所有的post请求。如不配置csrf，必须关闭。
 
     }
