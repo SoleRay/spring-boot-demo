06b659efbcf6265d48edc3a4fe87cdb57f2dfc3d
soleray <pein_zero@126.com>
2020-10-13 20:25:00 +0800

    update

---
+++ src/main/java/com/demo/config/oauth2/AuthorizationServerConfiguration.java
@@ -0,0 +1,40 @@
+package com.demo.config.oauth2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@EnableAuthorizationServer
+@Configuration
+public class AuthorizationServerConfiguration  extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("client")
+        .redirectUris("http://localhost:81/order")
+                .scopes("read","write")
+                .secret("secret")
+                .authorizedGrantTypes("authorization_code","password","implicit","client_credentials");}
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        super.configure(security);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+       endpoints.authenticationManager(authenticationManager);
+    }
+
+
+}
