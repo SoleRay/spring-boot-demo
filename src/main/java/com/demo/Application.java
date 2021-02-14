773f1266de829b2b74c4bc6ddf34f324a8b4c446
soleray <pein_zero@126.com>
2021-02-14 21:26:49 +0800

    Spring注解版本

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -11,12 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 或者你使用 @ComponentScan注解而不需要指定basePackage
  * 所以这里的@ComponentScan已经注释掉了
  *
- * 如果需要使用mybatis，简单的方式可以直接用@MapperScan标记，指明扫包的位置
- * 但由于我们使用了通用mybatis，所以这个扫包的方式交给TkMybatisConfig来完成
  */
 
 
-@tk.mybatis.spring.annotation.MapperScan(basePackages = "com.demo.dao")
 @SpringBootApplication
 public class Application {
 
