55147fbf05086ffedb22db0e8ea0217f464557ae
soleray <pein_zero@126.com>
2021-02-15 17:51:40 +0800

    Spring注解版本

--- src/main/java/com/demo/config/imports/selector/ConfigImportSelector.java
+++ src/main/java/com/demo/config/imports/selector/ConfigImportSelector.java
@@ -22,8 +22,10 @@ public abstract class ConfigImportSelector<A extends Annotation> implements Impo
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
 
+        //这个annType就是ConfigImportSelector<A> 中这个泛型A的类型，通常这个A就是Enable注解的类型
         Class<?> annType = GenericTypeResolver.resolveTypeArgument(getClass(), ConfigImportSelector.class);
 
+        //获取Enable注解类的属性，它的属性其实就是方法
         Map<String, Object> annotationAttributes = importingClassMetadata.getAnnotationAttributes(annType.getName(), false);
 
         if(annotationAttributes==null || annotationAttributes.isEmpty()){
@@ -31,6 +33,7 @@ public abstract class ConfigImportSelector<A extends Annotation> implements Impo
         }
 
         try{
+            //获取Enable注解类属性中的config属性，其实就是调用他的config()方法
             ConfigEnum configEnum = (ConfigEnum) annotationAttributes.get(getConfigName());
             switch (configEnum) {
                 case APPLE:
