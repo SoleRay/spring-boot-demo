e2494c4a69c5b15c88d0925d748542db1e932b5f
soleray <pein_zero@126.com>
2021-03-21 10:40:22 +0800

    change

--- src/main/java/com/demo/config/aop/demo/DemoBeforeAdvice.java
+++ src/main/java/com/demo/config/aop/demo/DemoBeforeAdvice.java
@@ -9,6 +9,6 @@ public class DemoBeforeAdvice implements MethodBeforeAdvice {
 
     @Override
     public void before(Method method, Object[] args, Object target) throws Throwable {
-        System.out.println(method.getName());
+//        System.out.println(method.getName());
     }
 }
--- src/main/java/com/demo/controller/demo/DemoController.java
+++ src/main/java/com/demo/controller/demo/DemoController.java
@@ -1,28 +1,48 @@
 package com.demo.controller.demo;
 
 import com.demo.anno.ResponseResult;
-import com.demo.bean.result.Result;
-import com.demo.bean.result.ResultCode;
 import com.demo.entity.demo.Demo;
-import com.demo.exception.BusinessException;
 import com.demo.service.demo.DemoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
 @RestController
 @ResponseResult
 @RequestMapping("/demo")
 public class DemoController {
 
+    private AtomicInteger count = new AtomicInteger(1);
+
     @Autowired
     private DemoService demoService;
 
     @PostMapping(value="/selectDemo")
-    public Demo selectDemo(String id){
-        Demo demo = demoService.selectByPrimaryKey(id);
+    public Demo selectDemo(Integer id){
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        log.info("count {}",count.getAndIncrement());
+//        if(log.isDebugEnabled()){
+//            log.debug("select demo start....");
+//        }
+//        Demo demo = demoService.selectByPrimaryKey(id);
+        Demo demo = new Demo();
         return demo;
+    }
+
+    @GetMapping(value="/reset")
+    public void reset(){
+        count.setRelease(1);
     }
 
     @PostMapping(value="/insertDemo")
@@ -34,5 +54,4 @@ public class DemoController {
     public void updateDemo(Demo demo){
         demoService.updateByPrimaryKey(demo);
     }
-
 }
